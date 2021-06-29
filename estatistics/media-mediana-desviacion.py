0a087699ac06b42d5da47550354a2318e35e50fa
stevencumbicos <stev-sca@hotmail.com>
2021-06-28 23:45:26 -0500

    mejora al codigo de desviación estándar

--- estatistics/media-mediana-desviacion.py
+++ estatistics/media-mediana-desviacion.py
@@ -1,13 +1,27 @@
 import numpy as np
+import math
 
 def run():
-    data = [18, 20, 21, 23, 25, 28, 32, 35, 39]
+    # data = [18, 20, 21, 23, 25, 28, 32, 35, 39]
+    data = [52,55,58]
+    print("----------------")
+
     print("Media: ", np.mean(data))
     print("Mediana: ", np.median(data))
-    print("Desviación Estándar: ", np.std(data))
-    print("Varianza: ", np.var(data))
 
+    print("----------------")
 
+    print("Varianza Población: ", np.var(data))
+    print("Desviación Estándar (población): ", np.std(data))
+
+    print("----------------")
+
+    print("Varianza Muestra: ", np.var(data, ddof=1))
+    de_muestra = np.var(data, ddof=1)
+    de_muestra = sqrt = de_muestra**(1/2)
+    print("Desviación Estándar (muestra): " + str(de_muestra))
+
+    print("----------------")
 
 
 if __name__ == "__main__":
