1e3d5355300bad123d81cb4041e3a51a57e8f5c3
stevencumbicos <stev-sca@hotmail.com>
2021-07-02 00:13:58 -0500

    ¿código espaguetti? JAMAS - Funciones ordenadas - Código escalable - Orden y legiblidad optimizada

--- estatistics/media-mediana-desviacion.py
+++ estatistics/media-mediana-desviacion.py
@@ -3,35 +3,33 @@ import math
 import pandas as pd
 import statistics as stat
 
-def run():
-    # data = [42, 47, 53, 47, 50, 45, 46, 48, 41, 49, 45, 40, 54]
-    # data = [5,6,15,4,2]
-    data = [25,28,30,30,35,35,36,37,37,38,40,40,40,40,40,40,41,43,48,50]
-    data.sort()
-    print(f"Los datos ordenados: ", data)
 
+def mean_median_mode(data):
     print("----------------")
-
     print("Media: ", np.mean(data))
     print("Mediana: ", np.median(data))
     print("Moda: ", stat.mode(data))
 
+# standard variance
+# and → standard population variance
+def standard_variance(data):
     print("----------------")
-
     print("Varianza Población: ", np.var(data))
     print("Desviación Estándar (población): ", np.std(data))
-
     print("----------------")
 
+
+# sample variance
+# and → sample standard variance
+def sample_variance(data):
     print("Varianza Muestra: ", np.var(data, ddof=1))
-    de_muestra = np.var(data, ddof=1)
-    de_muestra = sqrt=de_muestra**(1/2)
+    de_muestra = np.var(data, ddof = 1)
+    de_muestra = sqrt=de_muestra**(1 / 2)
     print("Desviación Estándar (muestra): " + str(de_muestra))
-
     print("----------------")
 
 
-    data = np.array([data])
+def deciles(data):
     deciles = np.percentile(data, np.arange(10,100,10))
     print(f"Deciles: {deciles}")
 
@@ -46,13 +44,20 @@ def run():
         "80%" : deciles[7],
         "90%" : deciles[8]
     }
-
     for key, value in dictionary.items():
-        print(key, "-", value)
-
+        print(key, "→", value)
     print("----------------")
 
 
+def run():
+    data = [25,28,30,30,35,35,36,37,37,38,40,40,40,40,40,40,41,43,48,50]
+    # data = [56,58,64,67,68,73,78,83,84,88,89,90,91,92,93,93,94,95,97,99]
+    data.sort()
+    print(f"Los datos ordenados: ", data)
+    mean_median_mode(data)
+    standard_variance(data)
+    sample_variance(data)
+    deciles(data)
 
 if __name__ == "__main__":
     run()
